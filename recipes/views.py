8680a624bed82a4766d71807bb571ea3f2d7fd6b
DGusFr <diegogustavo.fr@gmail.com>
2023-01-06 09:10:45 -0300

    add changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # HttpResponse é a resposta ao request
 def home(request):
     # return HttpResponse('HOME')
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Diego',
     })
