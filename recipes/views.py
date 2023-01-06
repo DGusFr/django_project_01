116a6f65d0d7c897b32802b60dae95907fa3d351
DGusFr <diegogustavo.fr@gmail.com>
2023-01-06 08:27:41 -0300

    add changes

--- recipes/views.py
+++ recipes/views.py
@@ -8,11 +8,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Diego',
     })
-
-
-def contato(request):
-    return render(request, 'temp.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
