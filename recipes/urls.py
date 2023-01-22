1bccf10a0090a5177f7d620633cccb7477de0130
DGusFr <diegogustavo.fr@gmail.com>
2023-01-22 20:25:51 -0300

    add changes

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     # <int:id> usamos para notificar qual receita queremos impimir
     # o int serve para permitir apenas numeros no link
-    path('recipes/<int:id>/', views.recipe),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
 
-# HttpResponse é a resposta ao request
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': [make_recipe() for _ in range(10)],
@@ -13,4 +12,5 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
